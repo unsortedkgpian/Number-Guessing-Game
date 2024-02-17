1d1d2d870d2df106c245121d2becaadc918f2201
Aditya Kumar <99820240+unsortedkgpian@users.noreply.github.com>
2024-02-18 00:57:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+
+# random.randrange(start,stop) stop is absolute upper bound
+
+top_of_range = input("Type  a number:  ")
+
+if top_of_range.isdigit():
+    top_of_range = int (top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+
+# print(random_number)
+
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess >= random_number:
+        print("You were above  the number")
+    else:
+        print("You were below the number!")
+
+print("You got it in" , str(guesses) + "th guesses")
